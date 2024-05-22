81180dd2e16bc3aa6b0502e0ad4542238bbc964a
dhanykoinworks <dhany.aritonang@koinworks.com>
2024-05-22 15:03:00 +0700

    feat: register a handler

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"os"
 
 	"go.uber.org/fx"
 )
 
 // NewHTTPServer build a HTTP server that will begin serving requests
 // when the Fx application starts
-func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			ln, err := net.Listen("tcp", srv.Addr)
@@ -32,9 +34,32 @@ func NewHTTPServer(lc fx.Lifecycle) *http.Server {
 	return srv
 }
 
+type EchoHandler struct{}
+
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+func (e *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stdout, "Failed to handle request: ", err)
+	}
+}
+
+func NewServeMux(ec *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", ec)
+	return mux
+}
+
 func main() {
 	fx.New(
-		fx.Provide(NewHTTPServer),        // register function
-		fx.Invoke(func(*http.Server) {}), // run function
+		fx.Provide(
+			NewHTTPServer,
+			NewEchoHandler,
+			NewServeMux,
+		), // provide: register function
+
+		fx.Invoke(func(*http.Server) {}), // invoke: run function
 	).Run()
 }
