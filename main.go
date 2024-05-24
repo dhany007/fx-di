0046f884fef79ee1da402d2cbbd32bf2479269fa
dhanykoinworks <dhany.aritonang@koinworks.com>
2024-05-24 15:56:44 +0700

    feat: register many handlers

--- main.go
+++ main.go
@@ -101,11 +101,22 @@ func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // NewServeMux builds a ServeMux that will route requests
 // to the given Route.
-func NewServeMux(route1, route2 Route) *http.ServeMux {
+func NewServeMux(routes []Route) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle(route1.Pattern(), route1)
-	mux.Handle(route2.Pattern(), route2)
+
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
+
 	return mux
+}
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
 }
 
 func main() {
@@ -117,18 +128,10 @@ func main() {
 			NewHTTPServer,
 			fx.Annotate(
 				NewServeMux,
-				fx.ParamTags(`name:"echo"`, `name:"hello"`),
+				fx.ParamTags(`group:"routes"`),
 			),
-			fx.Annotate(
-				NewEchoHandler,
-				fx.As(new(Route)), //cast its result to that interface
-				fx.ResultTags(`name:"echo"`),
-			),
-			fx.Annotate(
-				NewHelloHandler,
-				fx.As(new(Route)),
-				fx.ResultTags(`name:"hello"`),
-			),
+			AsRoute(NewEchoHandler),
+			AsRoute(NewHelloHandler),
 			zap.NewExample, // in production should use zap.NewProduction
 		), // provide: register function
 
